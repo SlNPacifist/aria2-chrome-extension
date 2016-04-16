c5c7f8cf7735a519b3a81d3bf81b44d9169011c2
slnpacifist <slnpacifist@gmail.com>
2016-04-17 00:43:52 +0800

    Moved source files to src folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,16 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    copy: {
-      main: {
-        files: [
-          {
-            expand: true,
-            src: [
-              './**/{LICENSE,*.js,*.html,*.css,*.png,*.json,*.svg,*.otf,*.eot,*.ttf,*.woff,*.woff2,*.ico}',
-              '!./webui-aria2/screenshots/**',
-              '!./**/.git/**',
-              '!./**/node_modules/**',
-              '!./dist/**',
-              '!./**/Gruntfile.{js,coffee}',
-              '!./**/package.json',
-              '!./webui-aria2/favicon.ico'
-            ],
-            dest: 'dist/'
-          },
-        ]
-      },
-    },
     sync: {
       main: {
         files: [
           {
+            cwd: 'src',
             src: [
-              './**/{LICENSE,*.js,*.html,*.css,*.png,*.json,*.svg,*.otf,*.eot,*.ttf,*.woff,*.woff2,*.ico}',
-              '!./webui-aria2/screenshots/**',
-              '!./**/.git/**',
-              '!./**/node_modules/**',
-              '!./dist/**',
-              '!./**/Gruntfile.{js,coffee}',
-              '!./**/package.json',
-              '!./webui-aria2/favicon.ico'
+              '**/{LICENSE,*.js,*.html,*.css,*.png,*.json,*.svg,*.otf,*.eot,*.ttf,*.woff,*.woff2,*.ico}',
+              '!webui-aria2/screenshots/**',
+              '!**/.git/**',
+              '!webui-aria2/favicon.ico'
             ],
             dest: 'dist/'
           },
