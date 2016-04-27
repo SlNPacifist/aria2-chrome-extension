09f7809782a8f06cfe23093a33e9ccaff5f18d98
slnpacifist <slnpacifist@gmail.com>
2016-04-27 20:23:48 +0800

    Added grunt tasks for minifying js,css,html. Added grunt task "combine" to do everything. Only needed flags are copied now. Only woff2 font is copied now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,19 +7,71 @@ module.exports = function(grunt) {
           {
             cwd: 'src',
             src: [
-              '**/{LICENSE,*.js,*.html,*.css,*.png,*.json,*.svg,*.otf,*.eot,*.ttf,*.woff,*.woff2,*.ico}',
+              '**/{LICENSE,*.png,*.json,*.svg,*.ico}',
               '!webui-aria2/screenshots/**',
               '!**/.git/**',
-              '!webui-aria2/favicon.ico'
+              '!webui-aria2/flags/**',
+              '!webui-aria2/fonts/**'
             ],
+            dest: 'dist/'
+          },
+          {
+            cwd: 'src',
+            src: [
+              'webui-aria2/flags/**/{us,th,nl,cn,pl,fr,de}.svg',
+              '!**/.git/**',
+            ],
+            dest: 'dist/'
+          },
+          {
+            cwd: 'src',
+            src: 'webui-aria2/fonts/fontawesome-webfont.woff2',
             dest: 'dist/'
           },
         ],
         verbose: true,
         updateAndDelete: true
       },
+    },
+    uglify: {
+      main: {
+        files: [{
+            expand: true,
+            cwd: 'src',
+            src: ['**/*.js', '!**/.git/**'],
+            dest: 'dist'
+        }]
+      }
+    },
+    cssmin: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.css', '!**/.git/**', '!webui-aria2/css/font-awesome.css'],
+          dest: 'dist'
+        }]
+      }
+    },
+    htmlmin: {
+      main: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.html', '!**/.git/**'],
+          dest: 'dist'
+        }]
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-sync');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.registerTask('combine', ['sync', 'uglify', 'cssmin', 'htmlmin']);
 }
